d148b6c62cf2a32a6d48511ba90c75807616a05e
Rigel222 <NoSpam2@Rigel222.com>
2017-10-07 20:14:23 -0400

    ruth only

--- boilerplate.php
+++ boilerplate.php
@@ -1,5 +1,11 @@
 <?php
-if (!isset($_REQUEST['ruth'])) {
-	header('location: ./comingsoon.php');
+session_start();
+if (isset($_REQUEST['ruth'])) {
+	$_SESSION['ruth']=true;
+} else {
+	if ($_SESSION['ruth']) {
+	} else {
+		header('location: ./comingsoon.php');
+	}
 }
 ?>
