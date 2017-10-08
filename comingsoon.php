e3971f6274f934e3278d1b8ccbf945418323e620
rigel222 <nospam2@Rigel222.com>
2017-10-08 08:09:18 +0800

    coming soon

---
+++ comingsoon.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Lobster|Roboto" rel="stylesheet">
+		<link href="css/bm.css" rel="stylesheet" type="text/css" />
+		<script src="js/bm.js"></script>
+	</head>
+	<body>
+		<div class="comingSoon">
+			<div>
+				<h1>Coming Soon!!</h1>
+				<div class="comingSoonLogo1">
+				</div>
+				<div class="comingSoonLogo2">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
